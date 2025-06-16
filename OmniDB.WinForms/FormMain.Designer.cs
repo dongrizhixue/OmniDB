cd50023211552f5d451cb27ec7b1029801082f8e
冬日 <853523391@qq.com>
2025-06-16 19:12:17 +0800

    完成了1和2两个功能 fix#1,fix#2

---
+++ OmniDB.WinForms/FormMain.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace OmniDB.WinForms
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            连接ToolStripMenuItem = new ToolStripMenuItem();
+            TSMINewConnection = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { 连接ToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 25);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // 连接ToolStripMenuItem
+            // 
+            连接ToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { TSMINewConnection });
+            连接ToolStripMenuItem.Name = "连接ToolStripMenuItem";
+            连接ToolStripMenuItem.Size = new Size(44, 21);
+            连接ToolStripMenuItem.Text = "连接";
+            // 
+            // TSMINewConnection
+            // 
+            TSMINewConnection.Name = "TSMINewConnection";
+            TSMINewConnection.Size = new Size(180, 22);
+            TSMINewConnection.Text = "新建连接";
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "FormMain";
+            Text = "数据连接工具";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem 连接ToolStripMenuItem;
+        private ToolStripMenuItem TSMINewConnection;
+    }
+}
